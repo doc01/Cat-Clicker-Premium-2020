a23404d83f267f0e81e181fceeabc93f1e2e67e5
DocHub001 <48736430+DocHub001@users.noreply.github.com>
2020-10-18 23:43:48 +0300

    - Added the Model (a data object) - Created the octopus to return cat name and cat id data to populates   the cat list - Created init function in the octopus to iniate the app - Added a View for the cat list, it call the octopus for the require   data - Call the init function at the end of the IFIE

--- app.js
+++ app.js
@@ -1,3 +1,75 @@
+(function () {
+
+  const data = {
+    currentCat: 0,
+    totalClicks: 0,
+    cats: [
+      {
+        name: 'Kitty Blue Eyes',
+        img: 'img/cat.jpg',
+        clicksCount: 0,
+        id: 0,
+      },
+      {
+        name: 'Kitty Kitty Kitty',
+        img: 'img/cat-2.jpg',
+        clicksCount: 0,
+        id: 1,
+      },
+      {
+        name: 'Tabby A and Tabby B',
+        img: 'img/Tabby%20A%20and%20Tabbt%20B.jpg',
+        clicksCount: 0,
+        id: 2,
+      },
+      {
+        name: 'Whitey Boss',
+        img: 'https://images.unsplash.com/photo-1546457028-48b8bc63fe98',
+        clicksCount: 0,
+        id: 3,
+      },
+      {
+        name: 'Random Cat from Unsplash',
+        img: 'img/https://source.unsplash.com/collection/139386',
+        clicksCount: 0,
+        id: 4,
+      },
+    ]
+  }
+
+  const octopus = {
+    catList: function () {
+      return data.cats.map(cat => ({name: cat.name, id: cat.id}));
+    },
+
+    init: function () {
+      viewList.catListDisplay();
+    }
+  }
+
+  const viewList = {
+
+    catListDisplay: function () {
+      const catNamesElm = document.querySelector('.cat-names');
+      const catNames = octopus.catList();
+      catNames.forEach((mehow, i) => {
+        const catName = document.createTextNode(mehow.name);
+        const li = document.createElement("li");
+        li.appendChild(catName);
+        li.classList = 'cat-name';
+        li.dataset.id = mehow.id;
+        catNamesElm.appendChild(li);
+      });
+    },
+  }
+
+  octopus.init();
+}());
+
+
+
+
+
 // Dom Eleements - UI
 const mehows = document.querySelectorAll('.mehow');
 const showClicked = document.querySelectorAll('.box-results__clicked');
@@ -12,16 +84,16 @@ let count = 1; // - Inital count used only once to check if it is the first clic
 let preCat; // - ID of the previous display cat on the screen.
 
 // Functions
-function gennerateCatslist() {
-  mehows.forEach(mehow => {
-    const li = document.createElement("li");
-    const catName = document.createTextNode(mehow.dataset.name);
-    li.appendChild(catName);
-    li.classList = 'cat-name';
-    li.dataset.id = mehow.dataset.id;
-    catsList.appendChild(li);
-  });
-};
+// function gennerateCatslist() {
+//   mehows.forEach(mehow => {
+//     const li = document.createElement("li");
+//     const catName = document.createTextNode(mehow.dataset.name);
+//     li.appendChild(catName);
+//     li.classList = 'cat-name';
+//     li.dataset.id = mehow.dataset.id;
+//     catsList.appendChild(li);
+//   });
+// };
 
 function gennerateCatnames() {
   mehows.forEach(mehow => {
@@ -81,5 +153,5 @@ function displayAllresults() {
 catsList.addEventListener('click', displayCat);
 mehows.forEach(mehow => mehow.addEventListener('click', countClicks));
 
-document.addEventListener("DOMContentLoaded", gennerateCatslist);
+// document.addEventListener("DOMContentLoaded", gennerateCatslist);
 document.addEventListener("DOMContentLoaded", gennerateCatnames);
