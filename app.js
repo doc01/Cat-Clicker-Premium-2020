9dcfea19883bcce4f29b538b50557d06e419a2de
DocHub001 <48736430+DocHub001@users.noreply.github.com>
2020-10-19 23:23:08 +0300

    - Added viewDisplay - Created displayCat function to show the current cat details - Created displayClicks to show the current cat click count - Created handleListClick in the octopus, which handle the data for the   displayCat function giving it the current cat data

--- app.js
+++ app.js
@@ -2,35 +2,41 @@
 
   const data = {
     currentCat: 0,
+    lastCat: null,
     totalClicks: 0,
     cats: [
       {
         name: 'Kitty Blue Eyes',
+        description: 'A photo of a cat',
         img: 'img/cat.jpg',
         clicksCount: 0,
         id: 0,
       },
       {
         name: 'Kitty Kitty Kitty',
+        description: 'A photo of a cat',
         img: 'img/cat-2.jpg',
         clicksCount: 0,
         id: 1,
       },
       {
         name: 'Tabby A and Tabby B',
+        description: 'A photo of a cat',
         img: 'img/Tabby%20A%20and%20Tabbt%20B.jpg',
         clicksCount: 0,
         id: 2,
       },
       {
         name: 'Whitey Boss',
+        description: 'A photo of a cat',
         img: 'https://images.unsplash.com/photo-1546457028-48b8bc63fe98',
         clicksCount: 0,
         id: 3,
       },
       {
         name: 'Random Cat from Unsplash',
-        img: 'img/https://source.unsplash.com/collection/139386',
+        description: 'A photo of a cat',
+        img: 'https://source.unsplash.com/collection/139386',
         clicksCount: 0,
         id: 4,
       },
@@ -39,18 +45,33 @@
 
   const octopus = {
     catList: function () {
-      return data.cats.map(cat => ({name: cat.name, id: cat.id}));
+      return data.cats.map(cat => ({ name: cat.name, id: cat.id }));
+    },
+
+    handleListClick: function (e) {
+      currentCat = parseInt(e.target.dataset.id);
+      lastCat = currentCat;
+      const currentCatData = data.cats.filter(cat => cat.id === currentCat);
+      viewDisplay.displayCat(...currentCatData);
     },
 
     init: function () {
-      viewList.catListDisplay();
+      viewList.init();
+      viewDisplay.init();
     }
   }
 
   const viewList = {
 
+    init: function () {
+      // Cache the dom
+      this.catNamesElm = document.querySelector('.cat-names');
+      viewList.catListDisplay();
+      // Events Listiners
+      this.catNamesElm.addEventListener('click', octopus.handleListClick);
+    },
+
     catListDisplay: function () {
-      const catNamesElm = document.querySelector('.cat-names');
       const catNames = octopus.catList();
       catNames.forEach((mehow, i) => {
         const catName = document.createTextNode(mehow.name);
@@ -58,13 +79,59 @@
         li.appendChild(catName);
         li.classList = 'cat-name';
         li.dataset.id = mehow.id;
-        catNamesElm.appendChild(li);
+        this.catNamesElm.appendChild(li);
       });
+    },
+  }
+
+  const viewDisplay = {
+
+    init: function () {
+      // Cache the dom
+      this.catTemplate = document.querySelector('#catDisplayTemplate');
+      this.catsContainer = document.querySelector('.main-content .cats');
+      this.welcomeMessage = document.querySelector('.intro__message');
+      this.results;
+    },
+
+    displayCat: function (catData) {
+      (welcomeMessage.style.display === '') ? fadeOut(this.welcomeMessage) : '';
+      catTemplate = this.catTemplate.innerHTML;
+      catsContainer = this.catsContainer;
+      catTemplate = catTemplate.replace(/{{catImg}}/g, catData.img);
+      catTemplate = catTemplate.replace(/{{catName}}/g, catData.name);
+      catTemplate = catTemplate.replace(/{{catId}}/g, catData.id);
+      catTemplate = catTemplate.replace(/{{catDescription}}/g, catData.description);
+      catsContainer.innerHTML = catTemplate;
+      const cat = catsContainer.children[0];
+      this.result = document.querySelector('.box-results__clicked');
+      this.displayClicks(catData.clicksCount);
+      setTimeout(() => { cat.classList.add("fadein") }, 0.45);
+    },
+
+    displayClicks: function (clicks) {
+      const results = this.result;
+      console.log(results)
+      const btn = document.querySelector('.btn');
+      if(clicks > 0 ) {
+        results.textContent = clicks;
+        btn.style.display = 'block';
+      } 
     },
   }
 
   octopus.init();
 }());
+
+
+function fadeOut(elm) {
+  elm.style.opacity = 0;
+  elm.style.transition = 'opacity 0.45s ease-out';
+
+  setTimeout(() => {
+    elm.style.display = 'none';
+  }, 350);
+}
 
 
 
@@ -150,7 +217,7 @@ function displayAllresults() {
 
 
 // Events Listiners
-catsList.addEventListener('click', displayCat);
+// catsList.addEventListener('click', displayCat);
 mehows.forEach(mehow => mehow.addEventListener('click', countClicks));
 
 // document.addEventListener("DOMContentLoaded", gennerateCatslist);
