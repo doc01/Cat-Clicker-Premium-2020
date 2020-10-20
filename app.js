47ce2d3eea170ebee6fa9ba0c13b7f4cc32fa489
DocHub001 <48736430+DocHub001@users.noreply.github.com>
2020-10-21 00:08:44 +0300

    Created handleImgClick function Modify DisplayClicks function to handle the button Created displayAllClicks function First version of Cat Clicker Premium is ready to launch!

--- app.js
+++ app.js
@@ -36,7 +36,7 @@
       {
         name: 'Random Cat from Unsplash',
         description: 'A photo of a cat',
-        img: 'https://source.unsplash.com/collection/139386',
+        img: 'https://source.unsplash.com/640x454/?cat',
         clicksCount: 0,
         id: 4,
       },
@@ -53,6 +53,18 @@
       lastCat = currentCat;
       const currentCatData = data.cats.filter(cat => cat.id === currentCat);
       viewDisplay.displayCat(...currentCatData);
+    },
+
+    handleImgClick: function (id) {
+      data.cats.forEach(cat => {
+        if (cat.id === id) {
+          ++cat.clicksCount;
+          cat.clicksCount === 1 ? viewDisplay.displayClicks(cat.clicksCount, true) : viewDisplay.displayClicks(cat.clicksCount, false);
+        }
+      });
+      let allClicks = 0;
+      data.cats.forEach(cat => allClicks += cat.clicksCount);
+      viewDisplay.displayAllClicks(allClicks);
     },
 
     init: function () {
@@ -92,10 +104,12 @@
       this.catsContainer = document.querySelector('.main-content .cats');
       this.welcomeMessage = document.querySelector('.intro__message');
       this.results;
+      this.img;
+      this.allResults;
     },
 
     displayCat: function (catData) {
-      (welcomeMessage.style.display === '') ? fadeOut(this.welcomeMessage) : '';
+      (this.welcomeMessage.style.display === '') ? fadeOut(this.welcomeMessage) : '';
       catTemplate = this.catTemplate.innerHTML;
       catsContainer = this.catsContainer;
       catTemplate = catTemplate.replace(/{{catImg}}/g, catData.img);
@@ -103,22 +117,34 @@
       catTemplate = catTemplate.replace(/{{catId}}/g, catData.id);
       catTemplate = catTemplate.replace(/{{catDescription}}/g, catData.description);
       catsContainer.innerHTML = catTemplate;
-      const cat = catsContainer.children[0];
+      // Adding the cat already clicks
       this.result = document.querySelector('.box-results__clicked');
-      this.displayClicks(catData.clicksCount);
+      catData.clicksCount > 0 ? this.displayClicks(catData.clicksCount, true) : '';
+      // Fading in the cat
+      const cat = catsContainer.children[0];
       setTimeout(() => { cat.classList.add("fadein") }, 0.45);
+      // Creating event listiner for the image
+      this.img = document.querySelector('.col .mehow');
+      const imgId = parseInt(this.img.dataset.id);
+      this.img.addEventListener('click', () => octopus.handleImgClick(imgId));
     },
 
-    displayClicks: function (clicks) {
+    displayClicks: function (clicks, firstTime) {
       const results = this.result;
-      console.log(results)
-      const btn = document.querySelector('.btn');
-      if(clicks > 0 ) {
-        results.textContent = clicks;
+      results.textContent = clicks;
+      if (firstTime === true) {
+        const btn = document.querySelector('.btn');
         btn.style.display = 'block';
-      } 
+      }
     },
+
+    displayAllClicks: function (allClicks) {
+      this.allResults = document.querySelector('.all-results');
+      const allResults = this.allResults;
+      allResults.textContent = allClicks;
+    }
   }
+
 
   octopus.init();
 }());
@@ -131,94 +157,4 @@ function fadeOut(elm) {
   setTimeout(() => {
     elm.style.display = 'none';
   }, 350);
-}
-
-
-
-
-
-// Dom Eleements - UI
-const mehows = document.querySelectorAll('.mehow');
-const showClicked = document.querySelectorAll('.box-results__clicked');
-const btn = document.querySelectorAll('.btn');
-const catsList = document.querySelector('.cat-names');
-const catsDisplay = document.querySelectorAll('.cat');
-const welcomeMessage = document.querySelector('.intro__message');
-const results = document.querySelectorAll('.box-results__clicked');
-const showAllresults = document.querySelector('.all-results');
-// Utiles (functions helpers)
-let count = 1; // - Inital count used only once to check if it is the first click.
-let preCat; // - ID of the previous display cat on the screen.
-
-// Functions
-// function gennerateCatslist() {
-//   mehows.forEach(mehow => {
-//     const li = document.createElement("li");
-//     const catName = document.createTextNode(mehow.dataset.name);
-//     li.appendChild(catName);
-//     li.classList = 'cat-name';
-//     li.dataset.id = mehow.dataset.id;
-//     catsList.appendChild(li);
-//   });
-// };
-
-function gennerateCatnames() {
-  mehows.forEach(mehow => {
-    const h2 = document.createElement("h2");
-    const catName = document.createTextNode(mehow.dataset.name);
-    h2.appendChild(catName);
-    mehow.parentNode.insertBefore(h2, mehow);
-  })
 };
-
-function fadeWelcomemessge() {
-  welcomeMessage.style.opacity = 0;
-  welcomeMessage.style.transition = 'opacity 0.35s ease-in-out';
-
-  setTimeout(() => {
-    welcomeMessage.style.display = 'none';
-  }, 350);
-};
-
-function displayCat(e) {
-  (welcomeMessage.style.display === '') ? fadeWelcomemessge() : '';
-
-  if (preCat !== parseInt(e.target.dataset.id) && preCat !== undefined) {
-    catsDisplay[e.target.dataset.id].style.display = 'block';
-    catsDisplay[preCat].style.display = 'none';
-  } else if (preCat !== parseInt(e.target.dataset.id) && preCat === undefined) {
-    catsDisplay[e.target.dataset.id].style.display = 'block';
-  }
-  preCat = parseInt(e.target.dataset.id);
-};
-
-function countClicks() {
-  const id = parseInt(this.dataset.id);
-  btn[id].style.display = "inline-block";
-  if (isNaN(showClicked[id].textContent)) {
-    showClicked[id].textContent = count;
-  } else {
-    showClicked[id].textContent = Number(++showClicked[id].textContent);
-  }
-
-  displayAllresults();
-};
-
-function displayAllresults() {
-  let allClicks = 0; // - counter for all clicks.
-  const numbers = [...results].filter(result => !isNaN(parseInt(result.textContent)));
-  numbers.forEach(number => allClicks += parseInt(number.textContent));
-  // results.forEach((result) => {
-  //   isNaN(parseInt(result.textContent))
-  //     ? '' : allClicks += parseInt(result.textContent);
-  // });
-  showAllresults.textContent = allClicks;
-}
-
-
-// Events Listiners
-// catsList.addEventListener('click', displayCat);
-mehows.forEach(mehow => mehow.addEventListener('click', countClicks));
-
-// document.addEventListener("DOMContentLoaded", gennerateCatslist);
-document.addEventListener("DOMContentLoaded", gennerateCatnames);
